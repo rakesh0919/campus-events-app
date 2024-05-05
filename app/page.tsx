c5cde12d280a8bd5762376220ba1b87f6f0d0a0a
Izaac Tay <izaac@g.ucla.edu>
2024-05-04 22:21:21 -0700

    updated title and description

--- app/page.tsx
+++ app/page.tsx
@@ -33,29 +33,36 @@ export default async function Home() {
       <div className="mb-16 mt-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <h2 className="text-2xl">Upcoming Events</h2>
       </div>
+      <section>
+        <div>
+          <h2 className="mb-3 text-2xl font-semibold">
+            
+          </h2>
 
-      <div className="mb-32 grid text-center space-x-4 lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {events.map((event) => (
-          <Link
-          href={"/event" + '/' + event._id }
-          className="group rounded-lg border border-neutral-700 px-5 py-4 transition-colors  hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          key={event._id}
-          
-        >
-          <div className="h-1/3 mb-10">
-            <h2 className="mb-3 text-2xl font-semibold">
-              {event.name}{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              
-              </span>
-            </h2>
-          </div>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            {event.description}
-          </p>
-        </Link>
-        ))}
-      </div>
+        </div>
+        <div className="mb-32 grid text-center space-x-4 lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+          {events.map((event: any) => (
+            <Link
+            href={"/event" + '/' + event._id }
+            className="group rounded-lg border border-neutral-700 px-5 py-4 transition-colors  hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            key={event._id}
+            
+          >
+            <div className="h-1/3 mb-10">
+              <h2 className="mb-3 text-2xl font-semibold">
+                {event.name}{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                
+                </span>
+              </h2>
+            </div>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              {event.description}
+            </p>
+          </Link>
+          ))}
+        </div>
+      </section>
 
       
       
